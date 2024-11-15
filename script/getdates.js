5cac1c87fc33359b070292e5e2591e27c2a2a977
Luis Moreno <luism@Luiss-Air.attlocal.net>
2024-11-14 23:21:12 -0500

    profile site

---
+++ script/getdates.js
@@ -0,0 +1,8 @@
+
+// For the current year
+const year = new Date().getFullYear();
+document.getElementById("year").textContent = year;
+
+// For the last modified date
+const lastModified = document.lastModified;
+document.getElementById("lastModified").textContent = lastModified;
